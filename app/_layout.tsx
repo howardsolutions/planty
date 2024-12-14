4e389aa69a0c3f5977bfc169a90713d4cdf23c67
howardsolutions <phungthinh0946519356@gmail.com>
2024-12-14 15:09:50 +0700

    feat UI

---
+++ app/_layout.tsx
@@ -0,0 +1,40 @@
+import { FontAwesome5 } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { theme } from "../theme";
+import { Feather } from "@expo/vector-icons";
+
+export default function Layout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping list",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "My idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
